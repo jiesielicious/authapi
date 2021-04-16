ba45545441ccfe6044a2b2401b2cb7d066338330
Jiesie Lota <jiesielota.rmf@gmail.com>
2021-04-16 22:32:44 +0800

    Added Verify User

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -75,6 +75,41 @@ class LoginController extends Controller
         return $user->verification_pin;
     }
 
+    public function verify(Request $request){
+        $result = array();
+
+        $request->validate([
+            "email" => "required",
+            "verification_pin" => "required",
+        ]);
+
+        $collection = User::where('email', $request->email)->where('verification_pin', $request->verification_pin)->first();
+        
+        if( !empty( $collection ) ):
+
+            if( !$this->checkIfUserRegistered( $collection->id ) ):
+                $user = User::where( 'id', $collection->id )->first();
+                $user->email_verified_at = NOW();
+                $user->save();
+
+                $result["message"] = 'User Registered Successfully!';
+            else:
+                $result["message"] = 'User is already Registered!';
+            endif;
+            
+        else:
+            $result["message"] = 'Error Registering User!';
+        endif;
+
+        return response()->json($result);
+    }
+
+
+    public function checkIfUserRegistered( $id ) {
+        $user = User::find($id)->first()->pluck('email_verified_at');
+        return ( $user ) ? true : false;
+    }
+
     //Send Invitation Link with Router/API URL for Signup
     //Insert User Details with Verification PIN
     //Send Email with Verification PIN
--- routes/api.php
+++ routes/api.php
@@ -27,4 +27,5 @@ Route::middleware('auth:sanctum')->group(function () {
         return $request->user();
     });
     Route::post('/signup', ['App\Http\Controllers\LoginController', 'signup']);
+    Route::post('/verify', ['App\Http\Controllers\LoginController', 'verify']);
 });
